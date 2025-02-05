ce5bde53e0ecbee0ae6f81b104d591c5802a269a
Lucas Costa Fontes <lucas.cf11@aluno.ifsc.edu.br>
2025-02-05 17:56:23 -0300

    Problemas com a decodificação no receptor QAM

--- LAB5/Circuito_Transmissor_QAM.m
+++ LAB5/Circuito_Transmissor_QAM.m
@@ -7,7 +7,7 @@ info = [1 0 1 1 0 0 0 0 0 1 1 0 0 0 1 0];
 bits = log2(M);
 fc = 10;
 
-periodos_pulso_NRZ = 4;
+periodos_pulso_NRZ = 50;
 amostras_periodo = 10;
 N = periodos_pulso_NRZ * amostras_periodo;
 A = 1;
@@ -44,11 +44,11 @@ seno = A*sin(2 * pi * fc * t);
 
 % Realizando as multiplicados
 sinalQAM_real_RZ_deslocado = cosseno .* sinalQAM_real_RZ;
-sinalQAM_imag_RZ_deslocado = seno .* sinalQAM_imag_RZ;
+sinalQAM_imag_RZ_deslocado = -seno .* sinalQAM_imag_RZ;
 
-sinal_final = sinalQAM_real_RZ_deslocado - sinalQAM_imag_RZ_deslocado;
+sinal_final_tx = sinalQAM_real_RZ_deslocado + sinalQAM_imag_RZ_deslocado;
 
-% Plots 
+% Plots da transmissão
 
 
 figure(1)
@@ -56,26 +56,82 @@ figure(1)
 subplot(211)
 plot(t, sinalQAM_real_RZ);
 title('RZ Real');
-xlabel('Tempo amostral'); ylim('Amplitude');
+xlabel('Tempo amostral'); ylabel('Amplitude');
 ylim([-4 4]);
 % Plot do RZ do imaginário
 subplot(212)
 plot(t, sinalQAM_imag_RZ);
 title('RZ Imaginário');
-xlabel('Tempo amostral'); ylim('Amplitude');
+xlabel('Tempo amostral'); ylabel('Amplitude');
 ylim([-4 4]);
 
 figure(2)
-plot(t,sinal_final);
+plot(t,sinal_final_tx);
 title('sinal final');
-xlabel('Tempo amostral'); ylim('Amplitude');
+xlabel('Tempo amostral'); ylabel('Amplitude');
+
+
+%-----------------------------------------------------------------------------------------------------------------
+% Realizando a recepção
+%-----------------------------------------------------------------------------------------------------------------
+
+
+% Recebendo o sinal final do transmissor
+sinal_inicial_rx = sinal_final_tx;
+
+% Colocando o sinal em banda base
+sinal_rx_real = cosseno .* sinal_inicial_rx;
+sinal_rx_imag = -seno .* sinal_inicial_rx;
+
+% Preparando o filtro passa baixas
+filtro_passa_baixas =  fir1(10, (fc*2)/fs);
+
+
+% Filtrando o sinal para deixar fidedigno ao original em banda base
+sinal_rx_real_filtrado = filter(filtro_passa_baixas, 1, sinal_rx_real); 
+sinal_rx_imag_filtrado = filter(filtro_passa_baixas, 1, sinal_rx_imag); 
+
+% Realizando as amostragens
+sinal_rx_real_amostrado = sinal_rx_real_filtrado(N:N:end);
+sinal_rx_imag_amostrado = sinal_rx_imag_filtrado(N:N:end);
+
+
+% Plotando tudo no domínio da frequência
+
+% Vetor da frequência:
+df = length(t) / (fs+1);
+f = -fs/2 : df : fs/2-1;
+
+X_ftx = fft(sinal_final_tx)/length(sinal_final_tx);
+X_ftx = fftshift(X_ftx);
+
+figure(3)
+plot(f,abs(X_ftx));
+xlim([-300 300]);
+ylim([0 4]);
+ylabel('Amplitude (Volts)');
+xlabel('f(Hz)');
+title('Sinal x1 em função da frequência');
+grid on;
 
 
 
 
 
 
-
-
+# % Plots da recepção
+# figure(3)
+# % Plot do RZ do real
+# subplot(211)
+# plot(t, sinal_rx_real_filtrado);
+# title('RZ Real');
+# xlabel('Tempo amostral'); ylabel('Amplitude');
+# ylim([-4 4]);
+# % Plot do RZ do imaginário
+# subplot(212)
+# plot(t, sinal_rx_imag_filtrado);
+# title('RZ Imaginário');
+# xlabel('Tempo amostral'); ylabel('Amplitude');
+# ylim([-4 4]);
 
 
