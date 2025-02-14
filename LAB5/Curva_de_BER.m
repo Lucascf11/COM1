38ca9e4b50f355cb4a3fa4090498a0207b603d02
Lucas Costa Fontes <lucas.cf11@aluno.ifsc.edu.br>
2025-02-14 14:59:35 -0300

    Adicionando curva BER

--- LAB5/Curva_de_BER.m
+++ LAB5/Curva_de_BER.m
@@ -6,20 +6,20 @@ pkg load signal;
 pkg load communications;
 
 function [sinal_normalizado] = modnorm(sinal_QAM)
-    % Calcula a energia média dos símbolos da constelação
-    energia_media = mean(abs(sinal_QAM).^2);
-    
-    % Calcula o fator de normalização para energia unitária
-    fator_normalizacao = 1 / sqrt(energia_media);
-    
-    % Normaliza o sinal QAM
-    sinal_normalizado = sinal_QAM * fator_normalizacao;
-  end  
+  % Calcula a energia média dos símbolos da constelação
+  energia_media = mean(abs(sinal_QAM).^2);
+  
+  % Calcula o fator de normalização para energia unitária
+  fator_normalizacao = 1 / sqrt(energia_media);
+  
+  % Normaliza o sinal QAM
+  sinal_normalizado = sinal_QAM * fator_normalizacao;
+end  
 
 LIMIT_SNR = 10;
 
 M = 16;
-Simbolos = 100000
+Simbolos = 100000;
 info = randint(1,Simbolos,M); % Gerando informação binária aleatória de M bits
 sinal_QAM = qammod(info,M);
 sinal_QAM_normalizado = modnorm(sinal_QAM);
@@ -28,7 +28,7 @@ sinal_QAM_normalizado = modnorm(sinal_QAM);
 % Função personalizada para calcular erros de bit
 for SNR = 0:LIMIT_SNR
     SNR
-    sinal_QAM_ruido = awgn(sinal_QAM_normalizado, SNR);
+    sinal_QAM_ruido = awgn(sinal_QAM_normalizado, SNR, 'measured');
     info_demod = qamdemod(sinal_QAM_ruido,M);
     [numErrors(SNR+1),taxa(SNR+1)] = symerr(info,info_demod);
 end
